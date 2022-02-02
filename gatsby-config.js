9f3fc40cb10750cff8ec27a84cb253e5475615da
steezeman <dave.cutting@inertialsense.com>
2022-02-01 22:58:18 -0700

    Recovery point

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,33 @@
 module.exports = {
-    siteMetadata: {
-      title: `cuttingcreates`,
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    title: `cuttingcreates`,
+    siteUrl: `https://www.cuttingcreates.com`,
+    siteTitle: `Dave Cutting`,
+    // Default title of the page
+    siteTitleAlt: `Dave Cutting`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Cutting Creates - Dave Cutting's Design Portfolio`,
+    // Used for SEO
+    siteDescription: `TODO: Add site description`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `Dave Cutting`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-emilia`,
+      options: {
+        name: `Dave Cutting`,
+        socialMedia: [
+          {
+            title: `Instagram`,
+            href: `https://www.instagram.com/`
+          }
+        ]
+      }
     },
-    plugins: [
-
-    ]
+  ]
 }
